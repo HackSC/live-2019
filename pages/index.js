c4e6f8ac4220ca7bf7fb09f93ef90418b2ccd9ed
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-04-01 11:05:41 -0700

    Added SASS support to project + Bulma

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
+import '../styles/stylesheet.scss'
+
 import Link from 'next/link'
 
 const Index = () => (
   <div>
-    <p>Hello HackSC</p>
+    <section className="hero is-primary is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Hello HackSC</h1>
 
-    <Link href='/api'>API</Link>
-    <Link href='/map'>Map</Link>
-    <Link href='/faq'>FAQ</Link>
+          <Link href='/api'><button className='button is-white'>API Directory</button></Link><br/>
+          <Link href='/map'><button className='button is-white'>Map</button></Link><br/>
+          <Link href='/faq'><button className='button is-white'>FAQ</button></Link>
+        </div>
+      </div>
+    </section>
   </div>
 )
 
