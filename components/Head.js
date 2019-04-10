19338a5b33ba9c080e8cdd1129267f32d971b017
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-04-09 18:21:44 -0700

    Added Google Analytics code

--- components/Head.js
+++ components/Head.js
@@ -16,6 +16,15 @@ class CustomHead extends Component {
 
         <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
 
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127488741-1"></script>
+        <script dangerouslySetInnerHTML={
+          {__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-127488741-1');
+          `}} />
+
         <link rel="icon" type="image/png" href="/static/favicon.png" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"/>
       </Head>
