499d4dd8afa18c587d4a53bcc7489839d998c694
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-04-10 22:57:24 -0700

    Updated map descriptions w/ Fedjaisms

--- pages/map.js
+++ pages/map.js
@@ -17,68 +17,68 @@ const Map = () => (
 
         <br/>
         <br/>
-        
+
         <div className="columns is-multiline">
           <div className="column is-one-third">
             <h3 className="is-size-3">Resting Area</h3>
             <p>
-              Rest up here! We have reserved a section of our floor space for naps, sleeping, and rest.
+              Rest up here! We have a designated floor space for you to repose, nod off, or full-on sleep.
             </p>
           </div>
 
           <div className="column is-one-third">
             <h3 className="is-size-3">Hacker Lounge</h3>
             <p>
-              The hacker lounge is the central space for snacks, MLH, and taking breaks. We'll have a few couches, MLH tables, a photobooth, and snacks for your enjoyment.
+              The hacker lounge is our central space for snacks, MLH, and light breaks. We'll have  couches, MLH tables, a photobooth, and various snacks and refreshments to enjoy.
             </p>
           </div>
 
           <div className="column is-one-third">
             <h3 className="is-size-3">Fun Area</h3>
             <p>
-              Be on the lookout for fun events throughout the course of the hackathon! We'll be hosting fun events like Soylent Pong, a Nerf Tournament, and more
+              Be on the lookout for fun events throughout the course of the hackathon! We'll be hosting fun events like Soylent Pong (you read that right), a Nerf Tournament, and much.
             </p>
           </div>
 
           <div className="column is-one-third">
             <h3 className="is-size-3">Cafe Area</h3>
             <p>
-              Lounge around here and take a break from the mayhem of the central hacking area. We'll have some nice tables and resting spaces for you with some great views of DTLA!
+              Take another break, you earned it, and catch a breath from the mayhem of the central hacking area. We'll have top-of-the-line tables and resting spaces with some great views of DTLA!
             </p>
           </div>
 
           <div className="column is-one-third">
             <h3 className="is-size-3">Main Workshop Area</h3>
             <p>
-              We'll have several workshops throughout the event. Come by and learn from our several workshop hosts who'll be talking about everything from cloud technologies to the blockchain
+              We'll have several workshops throughout the event. Come by and learn from our several workshop hosts who'll talk about everything from cloud technologies to blockchain.
             </p>
           </div>
 
           <div className="column is-one-third">
             <h3 className="is-size-3">Hacker + Sponsor Space</h3>
             <p>
-              This is where the magic happens! Claim your territory and let your creativity flourish. Hack here, talk to sponsors, and build something dope
+              This is where the magic happens! Claim your territory and let your creativity flourish. Hack with friends, talk with sponsors, and build something worth sharing.
             </p>
           </div>
 
           <div className="column is-one-third">
             <h3 className="is-size-3">Food Serving Area</h3>
             <p>
-              For food serving, we'll be using a wave system to distribute food throughout the event. Once your color has been called, head to the food serving area and fill your stomach up!
+              The last thing we want is hundreds of hangry hackers. For food serving, we'll be using a wave system to distribute food throughout the event. Once your color has been called, head to the food serving area and fill your stomach up!
             </p>
           </div>
 
           <div className="column is-one-third">
             <h3 className="is-size-3">Elevators</h3>
             <p>
-              We're located on the 13th floor of the California Market Center. If you need to head to the first floor, use these elevators
+              We're located on the 13th floor of the California Market Center. If you need to head to the first floor, use these elevators. There are no secret shortcuts, so don't try and find any.
             </p>
           </div>
 
           <div className="column is-one-third">
             <h3 className="is-size-3">IT Zone + Info Desk</h3>
             <p>
-              Need to download a large file? Come by the IT Zone for access to fast internet. In addition, we'll have info desks available in case you want to chat with an organizer and/or ask questions.
+              Need to download a large file? Come by the IT Zone for access to fast internet. Additionally, we'll have info desks available in case you want to chat with an organizer and/or ask questions.
             </p>
           </div>
         </div>
