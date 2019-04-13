6b07cfe38a555074d4fce66a1834c815a09822c9
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-04-12 21:00:23 -0700

    Content update

--- components/Countdown.js
+++ components/Countdown.js
@@ -1,17 +1,17 @@
 import Countdown from 'react-countdown-now';
 
 const CountdownDisplay = (props) => {
-  const startDate = new Date("04/12/2019 9:00 PM")
+  const endDate = new Date("04/14/2019 9:00 AM")
 
   return (
     <div className="countdown">
       <div className="columns is-vcentered">
         <div className="column is-hidden-mobile">
-          <span>Happening Now: <b>HackSC 2019!</b></span>
+          <span>HACKING Now: <b>HackSC 2019!</b></span>
         </div>
 
         <div className="column countdown-counter">
-          <span>Countdown: <b className="time"><Countdown date={startDate} /></b></span>
+          <span>Countdown: <b className="time"><Countdown date={endDate} /></b></span>
         </div>
       </div>
     </div>
